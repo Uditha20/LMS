a9eecd1af2cf31a36bcb750b89b05ecda9121d20
uditha <udithaindunil5@gmail.com>
2023-10-25 14:43:49 +0530

    lms

---
+++ index.php
@@ -0,0 +1,467 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>LMS</title>
+	<!-- Bootstrap -->
+	<link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+	<!-- icons -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+	<!-- external css -->
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<!-- font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,700;1,600&family=Roboto&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+	<section class="top  d-none d-md-block" style="height: 60px;">
+		<div class="container-fluid ">
+			<div class="icons d-flex justify-content-end ">
+				<i class="bi bi-youtube"> YouTube</i>
+				<i class="bi bi-facebook"> Facebook</i>
+				<i class="bi bi-whatsapp " style="margin-right:75px"> WhatsApp</i>
+			</div>
+		</div>
+	</section>
+
+
+	<div class="container-fluid">
+
+		<nav class="navbar navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand" href="#"><img src="./images/Hedaweda 2.png" alt="" width="80" height="100" class="img-fluid" /></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent1">
+				<ul class="navbar-nav  ml-auto">
+					<li class="nav-item active"> <a class="nav-link" href="#">Home</a> </li>
+					<li class="nav-item active"> <a class="nav-link" href="#">Course</a> </li>
+
+					<a href="./moodle/"><button type="button" class="btn btn-primary">Login</button></a>
+
+
+				</ul>
+
+			</div>
+		</nav>
+
+	</div>
+
+	<section class="hero">
+		<div class="container-fluid">
+			<div class="bg-image d-flex align-items-center justify-content-center text-center">
+				<div>
+					<h1 class="text-white mt-3">HEDAWEDA INSTITUTE</h1>
+					<div class="mt-3">
+						<h3 class="text-white">Sri Lanka's No 01 Educational & online Job opportunities paltform</h3>
+
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section><br>
+
+	<section class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-6 col-sm-6 col-12 mt-2 mb-3"> <!-- Adjust the column size to col-12 -->
+					<div class="card custom-card mt-4">
+						<img class="card-img-top img-fluid rounded" src="images/sorasak-_UIN-pFfJ7c-unsplash.jpg" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title">New User Registration</h5>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-6 col-12 mt-2 mb-3"> <!-- Adjust the column size to col-12 -->
+					<a href="./moodle/">
+						<div class="card custom-card mt-4">
+							<img class="card-img-top img-fluid rounded" src="images/sorasak-_UIN-pFfJ7c-unsplash.jpg" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title">Learning Management System</h5>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-6 col-12 mt-2 mb-3"> <!-- Adjust the column size to col-12 -->
+					<div class="card custom-card mt-4 	mb-4">
+						<img class="card-img-top img-fluid rounded" src="images/sorasak-_UIN-pFfJ7c-unsplash.jpg" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title">HEDAWEDA marketplace</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+
+	<br>
+	<section class="video-contant">
+		<!-- <div class="container">
+	
+	   <iframe  class="embed-responsive-item d-flex justify-content-center"  width="560" height="315" src="https://www.youtube.com/watch?v=DwuJeGYlYyw&list=RDDwuJeGYlYyw&start_radio=1" frameborder="0" allowfullscreen></iframe>
+	  
+	  </div> -->
+	</section>
+
+	<section class="about">
+		<div class="container">
+			<h1 class="text-center">About Us</h1><br>
+			<p class="text-justify">Our LMS is easy to use and offers a comprehensive set of features to support all aspects of the learning process. You can create and deliver
+				engaging courses, assess your progress, and track their results. HadaWeda Academy is also customizable to meet the specific needs of your
+				organization, and it is scalable to support a growing number of users and courses.</p><br>
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-sm-6 col-12 mt-2 mb-3">
+					<div class="card"> <img class="card-img-top img-fluid" src="./images/Feature Img (1).png" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title">Personalized learning</h5>
+
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 col-12 mt-2 mb-3">
+					<div class="card"> <img class="card-img-top img-fluid" src="images/fluent-emoji_man-teacher-light.png" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title">Best Teacher</h5>
+
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 col-12 mt-2 mb-3">
+					<div class="card"> <img class="card-img-top img-fluid" src="./images/fluent-emoji_globe-showing-europe-africa.png" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title">Learn from anywhere</h5>
+
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 col-12 mt-2 mb-3">
+					<div class="card"> <img class="card-img-top img-fluid" src="./images/Feature Img (1).png" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title">Test Your Knowledge</h5>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<br>
+
+
+	<section class="courses mt-4 d-none d-md-block" style="height:450px">
+		<h1 class="text-center pt-2 mb-4 pt-3">Our Courses</h1>
+
+		<div class="container">
+			<!-- Add your content here if needed -->
+		</div>
+
+		<div id="cardCarousel" class="carousel slide d-none d-md-block" data-ride="carousel">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#cardCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#cardCarousel" data-slide-to="1"></li>
+				<li data-target="#cardCarousel" data-slide-to="2"></li>
+			</ol>
+
+			<!-- Carousel Content -->
+			<div class="carousel-inner">
+				<div class="carousel-item mt-3 active">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-4  d-none d-md-block">
+								<div class="card">
+									<img src="./images/Rectangle 16 (2).png" class="card-img-top" alt="Image 1">
+									<div class="card-body">
+										<h5 class="card-title">Card 1</h5>
+										<p class="card-text">Description for Card 1.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4 d-none d-md-block">
+								<div class="card">
+									<img src="./images/Rectangle 16 (2).png" class="card-img-top" alt="Image 1">
+									<div class="card-body">
+										<h5 class="card-title">Card 1</h5>
+										<p class="card-text">Description for Card 1.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4 d-none d-md-block">
+								<div class="card">
+									<img src="./images/Rectangle 16 (2).png" class="card-img-top" alt="Image 1">
+									<div class="card-body">
+										<h5 class="card-title">Card 1</h5>
+										<p class="card-text">Description for Card 1.</p>
+									</div>
+								</div>
+							</div>
+							<!-- Add more cards for the first carousel item -->
+						</div>
+					</div>
+				</div>
+
+				<div class="carousel-item mt-3">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-4  d-none d-md-block">
+								<div class="card">
+									<img src="./images/Rectangle 16 (1).png" class="card-img-top" alt="Image 2">
+									<div class="card-body">
+										<h5 class="card-title">Card 2</h5>
+										<p class="card-text">Description for Card 2.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4 d-none d-md-block">
+								<div class="card">
+									<img src="./images/Rectangle 16 (1).png" class="card-img-top" alt="Image 2">
+									<div class="card-body">
+										<h5 class="card-title">Card 2</h5>
+										<p class="card-text">Description for Card 2.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4 d-none d-md-block">
+								<div class="card">
+									<img src="./images/Rectangle 16 (1).png" class="card-img-top" alt="Image 2">
+									<div class="card-body">
+										<h5 class="card-title">Card 2</h5>
+										<p class="card-text">Description for Card 2.</p>
+									</div>
+								</div>
+							</div>
+							<!-- Add more cards for the second carousel item -->
+						</div>
+					</div>
+				</div>
+
+				<!-- Add more carousel items with cards as needed -->
+			</div>
+
+			<!-- Controls -->
+			<a class="carousel-control-prev" href="#cardCarousel" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#cardCarousel" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</section>
+	<section class="services">
+		<div class="container">
+			<h1 class="text-center mb-4 mt-4">Our Services</h1>
+			<div class="row">
+				<div class="col-lg-6 text-justify">
+					<p>we offer a wide range of online courses to help you achieve vour learning goals.
+						Whether you're looking to advance your career, learn a new skill, or simply explore
+						your interests, we have a course for you.
+						Our courses are taught by experienced instructors and industry experts, and
+						they're designed to be engaging, informative, and effective, We offer a variety of
+						course formats to suit your learning style, including self-paced courses, instructor-
+						led courses, and blended learning courses.</p>
+				</div>
+				<div class="col-lg-6"><img src="images/Rectangle 2.png" alt="Placeholder image" class="img-fluid rounded"></div>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 mt-4"><img src="images/Rectangle 2.png" alt="Placeholder image" class="img-fluid rounded"></div>
+				<div class="col-lg-6">
+					<p class="text-justify mt-4">
+						we offer a wide range of online courses to help you achieve vour learning goals.
+						Whether you're looking to advance your career, learn a new skill, or simply explore
+						your interests, we have a course for you.
+						Our courses are taught by experienced instructors and industry experts, and
+						they're designed to be engaging, informative, and effective, We offer a variety of
+						course formats to suit your learning style, including self-paced courses, instructor-
+						led courses, and blended learning courses.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section class="feedback mt-5" style="height: 450px">
+		<div class="container">
+			<h1 class="text-center pt-4 mb-4">Our Feedbacks</h1>
+		</div>
+
+		<div id="feedbackCarousel" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#feedbackCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#feedbackCarousel" data-slide-to="1"></li>
+				<li data-target="#feedbackCarousel" data-slide-to="2"></li>
+				<li data-target="#feedbackCarousel" data-slide-to="3"></li>
+				<li data-target="#feedbackCarousel" data-slide-to="4"></li>
+			</ol>
+
+			<!-- Carousel Content -->
+			<div class="carousel-inner">
+				<!-- Loop through your feedback items -->
+				<div class="carousel-item active">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-6 mx-auto">
+								<div class="card d-flex justify-content-center align-items-center">
+									<img src="./images/Ellipse 1.png" class="card-img-top" alt="User Feedback 1">
+									<div class="card-body">
+										<h5 class="card-title">User Feedback 1</h5>
+										<p class="card-text">Here's what our users are saying about us.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-6 mx-auto">
+								<div class="card d-flex justify-content-center align-items-center">
+									<img src="./images/Ellipse 1.png " class="card-img-top" alt="User Feedback 2">
+									<div class="card-body">
+										<h5 class="card-title">User Feedback 2</h5>
+										<p class="card-text">Here's what another user is saying about us.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Add more carousel items for additional feedback items -->
+			</div>
+
+			<!-- Controls -->
+			<a class="carousel-control-prev" href="#feedbackCarousel" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#feedbackCarousel" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</section>
+
+
+
+
+
+
+	<section class="FAQ mt-4 mb-3">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<h3 class="mb-4 mt-3">Frequently Asked questions</h3>
+					<div id="accordion1" role="tablist">
+						<div class="card">
+							<div class="card-header" role="tab" id="headingOne1">
+								<h5 class="mb-0"> <a data-toggle="collapse" href="#collapseOne1" role="button" aria-expanded="true" aria-controls="collapseOne1"> Collapsible Group 1 > </a> </h5>
+							</div>
+							<div id="collapseOne1" class="collapse" role="tabpanel" aria-labelledby="headingOne1" data-parent="#accordion1">
+								<div class="card-body"></i>Content for Accordion Panel 1</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="headingTwo1">
+								<h5 class="mb-0"> <a class="collapsed" data-toggle="collapse" href="#collapseTwo1" role="button" aria-expanded="false" aria-controls="collapseTwo1"> Collapsible Group 2 ></a> </h5>
+							</div>
+							<div id="collapseTwo1" class="collapse" role="tabpanel" aria-labelledby="headingTwo1" data-parent="#accordion1">
+								<div class="card-body">Content for Accordion Panel 2</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="headingThree1">
+								<h5 class="mb-0"> <a class="collapsed" data-toggle="collapse" href="#collapseThree1" role="button" aria-expanded="false" aria-controls="collapseThree1"> Collapsible Group 3 > </a> </h5>
+							</div>
+							<div id="collapseThree1" class="collapse" role="tabpanel" aria-labelledby="headingThree1" data-parent="#accordion1">
+								<div class="card-body">Content for Accordion Panel 3</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<h3 class="mt-3">Give your suggestions</h3>
+
+					<form>
+						<div class="form-group">
+							<label for="exampleInputEmail1">Name</label>
+							<input type="text" class="form-control" id="exampleInputname" placeholder="Enter name">
+						</div>
+						<div class="form-group">
+							<label for="exampleInputEmail1">Email address</label>
+							<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+						</div>
+						<div class="form-group">
+							<label for="exampleInputEmail1">Phone number</label>
+							<input type="text" class="form-control" id="exampleInputnumber" placeholder="Enter number">
+						</div>
+
+
+						<button type="submit" class="btn btn-primary">Submit</button>
+
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="footer" style="background-color: #f8f9fa; padding: 50px 0;height:150px">
+		<div class="container-fluid">
+			<footer class="footer">
+				<div class="row">
+					<div class="col-lg-4 mb-4 mt-4">
+						<div class="d-flex justify-content-center align-items-center">
+							<img src="./images/logo-removebg-preview.png" class="img-fluid" alt="Placeholder image">
+						</div>
+					</div>
+					<div class="col-lg-4 mb-4 mt-4">
+						<div class="d-flex justify-content-center align-items-center">
+							<p>Address 1 <br>Address 2 <br>Address 3</p>
+						</div>
+					</div>
+					<div class="col-lg-4 mt-1">
+						<div class="icons d-flex  align-items-center">
+							<i class="bi bi-youtube" style="font-size: 2rem;"></i>
+							<span class="icon-label">YouTube</span>
+						</div>
+						<div class="icons d-flex  align-items-center">
+							<i class="bi bi-facebook" style="font-size: 2rem;"></i>
+							<span class="icon-label">Facebook</span>
+						</div>
+						<div class="icons d-flex align-items-center">
+							<i class="bi bi-whatsapp" style="font-size: 2rem;"></i>
+							<span class="icon-label">WhatsApp</span>
+						</div>
+					</div>
+
+				</div>
+		</div>
+		</footer>
+		</div>
+	</section>
+
+
+
+	</div>
+	</section>
+
+	<!-- body code goes here -->
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery-3.4.1.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap-4.4.1.js"></script>
+</body>
+
+</html>
